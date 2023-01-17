6aca1ad76ef965c6d966b6ce2d51730c7503478e
Wessel26 <119941904+Wessel26@users.noreply.github.com>
2023-01-17 14:50:09 +0100

    Versie 1.3

--- Assets/Code/Dialogue.cs
+++ Assets/Code/Dialogue.cs
@@ -1,86 +1,65 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-
 public class Dialogue : MonoBehaviour
 {
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
 
-    [System.Serializable]
-    public class DialogueSegment
-    {
-        public string SubjectText;
+    private int index;
 
-        [TextArea]
-        public string DialogueToPrint;
-        public bool Skippable;
-
-        [Range(1f, 25f)]
-        public float LettersPerSecond;
-    }
-    [SerializeField] private DialogueSegment[] DialogueSegments;
-    [Space]
-    [SerializeField] private TMP_Text SubjectText;
-    [SerializeField] private TMP_Text BodyText;
-
-    private int DialogueIndex;
-    private bool PlayingDialogue;
-    private bool Skip;
-   
+    // Start is called before the first frame update
     void Start()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (DialogueIndex == DialogueSegment.Lenght)
-            {
-                enabled = false;
-                return;
-            }
+        textComponent.text = string.Empty;
+        StartDialogue();
+    }
 
-            if (!PlayingDialogue)
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (textComponent.text == lines[index])
             {
-                StartCoroutine(PlayingDialogue(DialogueSegments[DialogueIndex]));
+                Nextline();
             }
             else
             {
-                if (DialogueSegments[DialogueIndex].Skippable)
-                {
-                    Skip = true;
-                }
+                StopAllCoroutines();
+                textComponent.text = lines[index];
             }
         }
     }
 
-    private IEnumerator PlayDialogue(DialogueSegment segment)
+    void StartDialogue()
     {
-        PlayingDialogue = true;
-
-        BodyText.SetText(string.Empty);
-        SubjectText.SetText(segment.SubjectText);
-
-        float delay = 1f / segment.LettersPerSecond;
-        for (int i = 0; i < segment.DialogueToPrint.Length; i++) 
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+ 
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
         {
-            if (Skip)
-            {
-                BodyText.SetText(segment.DialogueToPrint);
-                Skip = false;
-                break;
-            }
-
-            string chunkToAdd = string.Empty;
-            chunkToAdd += segment.DialogueToPrint[i];
-            if (segment.DialogueToPrint[i] == '' && i < segment.DialogueToPrint.Length -1)
-            {
-                chunkToAdd = segment.DialogueToPrint.Substring(if, 2);
-                i++;
-            }
-
-            BodyText.text += chunkToAdd;
-            yield return new WaitForSeconds(delay);
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
         }
-
-        PlayingDialogue = false;
-        DialogueIndex++;
-
+    }
+    
+    void Nextline()
+    {
+        if (index < lines.Length - 1) 
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
